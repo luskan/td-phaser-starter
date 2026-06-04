aef235a10600068a8495d103f366c821ecdce631
Marcin J. <marcinje@gmail.com>
2026-06-04 12:48:15 +0200

    Clean tutorial starter

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -3,10 +3,6 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameText: Phaser.GameObjects.Text;
-
     constructor ()
     {
         super('Game');
@@ -14,23 +10,15 @@ export class Game extends Scene
 
     create ()
     {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+        this.cameras.main.setBackgroundColor(0x244a5a);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+        this.add.text(512, 384, 'TD Phaser Starter', {
+            fontFamily: 'Arial',
+            fontSize: 40,
+            color: '#ffffff',
             align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        }).setOrigin(0.5);
 
         EventBus.emit('current-scene-ready', this);
-    }
-
-    changeScene ()
-    {
-        this.scene.start('GameOver');
     }
 }
